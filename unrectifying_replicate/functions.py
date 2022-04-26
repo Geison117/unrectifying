3f91299d73f0146a2170595efcdafae385d00f4b
Geison117 <geisonblanco94@gmail.com>
2022-04-26 11:32:35 -0500

    update parameter functions

--- unrectifying_replicate/functions.py
+++ unrectifying_replicate/functions.py
@@ -16,6 +16,73 @@
 # t
 import torch
 
+
+def update_W(p2, c1, U, b, V, MU2):
+    d, _ = b.size()
+    print()
+    _,J = U.size()
+    I = torch.eye(d)
+    sum1=0
+    sum2=0
+    for j in range(J):
+        u = torch.unsqueeze(U[:,j], 1)
+        v = torch.unsqueeze(V[:,j], 1)
+        mu2 = torch.unsqueeze(MU2[:,j], 1)
+        sum1 += p2*(u-b)@(v.T) + mu2@(v.T)
+        sum2 += torch.inverse(p2*v@(v.T)+c1*I)
+    W = sum1@sum2
+    return W
+
+def update_WL(c1, Y,b,V):
+    d, _ = b.size()
+    _, J = Y.size()
+    I = torch.eye(d)
+    sum1 = 0
+    sum2 = 0
+    for j in range(J):
+        y = torch.unsqueeze(Y[:, j], 1)
+        v = torch.unsqueeze(V[:, j], 1)
+        sum1 += (y - b) @ (v.T)
+        sum2 += torch.inverse( v @ (v.T) + c1 * I)
+    W = sum1 @ sum2
+    return W
+
+def update_b(p2, U, W, V, Mu2):
+    _, N = U.size()
+    sum1 = 0
+    for j in range(N):
+        u = torch.unsqueeze(U[:, j], 1)
+        v = torch.unsqueeze(V[:, j], 1)
+        mu2 = torch.unsqueeze(Mu2[:, j], 1)
+
+        sum1 += u - W@v + (1/p2)*mu2
+    b = sum1/N
+    return b
+
+b = update_b(1, torch.rand(2,6), torch.rand(2,2), torch.rand(2,6), torch.rand(2,6))
+
+def update_bL(Y, W, V):
+    _, N = Y.size()
+    sum1 = 0
+    for j in range(N):
+        y = torch.unsqueeze(Y[:, j], 1)
+        v = torch.unsqueeze(V[:, j], 1)
+
+        sum1 += y - W @ v
+    b = sum1 / N
+    return b
+
+bL = update_bL(torch.rand(2,6), torch.rand(2,2), torch.rand(2,6))
+
+def update_d(p1, p3, p4, c2, s, t, mu1, mu3, mu4, v, u):
+    num = (p1*v + p3*s + p4*(u+t) + mu1 - mu3 + mu4)*u
+    den = (1+p3+p4)*(u**2) + c2
+
+    d = torch.clip(torch.tensor(num/den), min =0, max=1)
+    return d.item()
+
+d = update_d(0.01, 0.6, 0.02, 1000, 0.01, 0.2, 0.3, 0.5, 0.69, 0.6, 0.6)
+
 def update_u(p1, p2, p3, p4, mu1, mu2, mu3, mu4, D, DL, v, vl_1, W, b, s, t):
     _, d = D.size()
     I = torch.eye(d)
@@ -34,7 +101,6 @@ def update_v(p1, p2, mu1, mu2, W, u, u_l1, b, D):
     _, d = D.size()
     I = torch.eye(d)
     v = torch.inverse(p2*W.T @ W + p1*I) @ (p2*W.T @ (u_l1 - b) + p1*D@u - mu1 + W.T@mu2)
-    #print(v)
     v = torch.clip(v, min =0, max=1)
     return v
 
@@ -45,11 +111,43 @@ def update_vL(p1, mu1, W, y, u, b, D):
     v = torch.clip(v, min =0, max=1)
     return v
 
+def update_s(p3, u, mu3, D):
+    s = D@u + (mu3/p3)
+    s = torch.clip(s, min =0, max=1)
+    return s
+
+def update_t(p4, u, mu4, D):
+    _, d = D.size()
+    I = torch.eye(d)
+    t = (D-I)@u - (mu4/p4)
+    t = torch.clip(t, min =0, max=1)
+    return t
+
+
+
 v = update_v(1, 2, torch.rand(2,1), torch.rand(2,1), torch.rand(2,2), torch.rand(2,1), torch.rand(2,1), torch.rand(2,1), torch.rand(2,2))
 vL =update_vL(1, torch.rand(2,1), torch.rand(2,2), torch.rand(2,1), torch.rand(2,1), torch.rand(2,1), torch.rand(2,2))
 
-print(vL)
-print(vL.size())
+
+def update_dual_variables(mu1, mu2, mu3, mu4, p1, p2, p3, p4, v, d, u, W, v_l1, b, t, s, i):
+    mu1 = mu1 + p1*(v-d*u)
+    op = u- W@v_l1 + b
+    mu2 = mu2 + p2*op[i].item()
+    mu3 = mu3 + p3*(d*u-s)
+    mu4 = mu4 + p4*((1-d)*u + t)
+
+    return mu1, mu2, mu3, mu4
+
+mu1, mu2, mu3, mu4 = update_dual_variables(1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, torch.rand(2,2), torch.rand(2,1), torch.rand(2,1), 2, 1, 0)
+
+
+
+
+print(d)
+print(d)
+
+
+
 #x = torch.rand(6, 5)
 #print(x)
 #
